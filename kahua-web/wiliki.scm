e244d93e805bfd4e94c4e49f0a2d05a206aff915
ENDO Yasuyuki <yasuyuki@javaopen.org>
2006-11-23 11:57:56 +0000

    move to plugins
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/kahua-web/trunk@2246 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- kahua-web/wiliki.scm
+++
@@ -1,9 +0,0 @@
-(allow-module wiliki.format)
-(guard (e (else #f))
-  (allow-module wiliki.page))
-(allow-module wiliki.log)
-(allow-module wiliki.pasttime)
-(allow-module util.lcs)
-(allow-module text.diff)
-(allow-module gauche.hook)
-
