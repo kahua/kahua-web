4b5db23c8fa78bacfa15e58d4827f3fd448de122
Tatsuya BIZENN <bizenn@gmail.com>
2007-04-18 09:25:19 +0000

    Use default database instead of "efs:kahua-web".
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/kahua-web/trunk@2637 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- init.scm
+++ init.scm
@@ -4,7 +4,7 @@
 ;;  Copyright (c) 2004 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: init.scm,v 1.2 2006/11/24 13:27:24 bizenn Exp $
+;; $Id: init.scm,v 1.3 2007/04/18 09:25:19 bizenn Exp $
 
 (use kahua)
 (use file.util)
@@ -14,9 +14,10 @@
 (define (main args)
   (let-args (cdr args)
       ((conf-file "c=s")
-       (site-dir "S=s"))
+       (site-dir "S=s")
+       (dbpath "D=s"))
     (kahua-common-init site-dir conf-file)
-    (let1 dbname (kahua-dbpath "efs:kahua-web")
+    (let1 dbname (kahua-dbpath (or dbpath (kahua-default-database-name)))
       (with-db (db dbname)
         (unless (find-kahua-instance <kahua-user> "admin")
           (let* ((pass (get-password "Type initial admin password: "))
