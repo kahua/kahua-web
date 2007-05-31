406e68f30ca3709d6f5c93c59912f5277ff6af32
Tatsuya BIZENN <bizenn@gmail.com>
2007-05-31 06:51:36 +0000

    Arrange code. Now copy standard plugins into site bundle.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/kahua-web/trunk@2769 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- init.scm
+++ init.scm
@@ -1,10 +1,10 @@
 ;; kahua-web initialization
 ;;
-;;  Copyright (c) 2004 Scheme Arts, L.L.C., All rights reserved.
-;;  Copyright (c) 2004 Time Intermedia Corporation, All rights reserved.
+;;  Copyright (c) 2004-2007 Scheme Arts, L.L.C., All rights reserved.
+;;  Copyright (c) 2004-2007 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: init.scm,v 1.3 2007/04/18 09:25:19 bizenn Exp $
+;; $Id: init.scm,v 1.4 2007/05/31 06:51:36 bizenn Exp $
 
 (use kahua)
 (use file.util)
@@ -20,18 +20,16 @@
     (let1 dbname (kahua-dbpath (or dbpath (kahua-default-database-name)))
       (with-db (db dbname)
         (unless (find-kahua-instance <kahua-user> "admin")
-          (let* ((pass (get-password "Type initial admin password: "))
-                 (u (kahua-add-user "admin" pass)))
-            (slot-set! u 'role-alist '(admin))))
-        ))
-    0))
+          (let1 pass (get-password "Type initial admin password")
+	    (kahua-add-user "admin" pass :role-alist '(admin)))))))
+  0)
 
 (define (get-password prompt)
   (let* ((port (current-input-port))
          (attr (sys-tcgetattr port))
          (lflag (slot-ref attr 'lflag)))
     ;; Show prompt
-    (display prompt)
+    (format #t "~a: " prompt)
     (flush)
     ;; Turn off echo during reading.
     (dynamic-wind
